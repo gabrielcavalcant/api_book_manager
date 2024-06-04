e39f5ce51bf182038610d86ae7faecc77ca240eb
RenanHS2RP <renanoliveiratm@hotmail.com>
2024-06-03 21:40:40 -0300

    feat(Repository): Repository created

---
+++ book_manager/book_manager/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace book_manager.Repository
+{
+    public class Repository
+    {
+        public DataTable ExecuteProcedure(string procedureName, SqlParameter[] parameters)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            DataTable dataSet = new DataTable();
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    SqlCommand command = new SqlCommand(procedureName, sqlConnection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    if (parameters == null)
+                    {
+                        command.Parameters.Add(new SqlDataAdapter(command));
+                    }
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataSet);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
+            }
+            return dataSet;
+        }
+    }
+}
