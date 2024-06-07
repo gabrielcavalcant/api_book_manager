98c637ce37465d822fd4dc20f4d85556cc21271f
RenanHS2RP <renanoliveiratm@hotmail.com>
2024-06-07 14:04:59 -0300

    feat(Business): all Book Business methods implemented

--- book_manager/book_manager/Business/Business.cs
+++ book_manager/book_manager/Business/Business.cs
@@ -1,28 +1,56 @@
-﻿using System;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Runtime;
+using book_manager.Models;
 using book_manager.Repository;
 
 namespace book_manager.Business
 {
     public class Business
     {
+        private readonly ImpRepository _repository;
+
         public Business()
         {
-
+            _repository = new ImpRepository();
         }
 
-        public DataTable GetCustumers(int id)
+        public DataTable GetBooks()
         {
-            ImpRepository _Repository = new ImpRepository();
-            SqlParameter[] parameter =
+            return _repository.ExecuteProcedure("[dbo].[prGET_BOOKS]", null);
+        }
+
+        public void AddBook(Livro livro)
+        {
+            SqlParameter[] parameters =
             {
-                new SqlParameter("id", id),
+                new SqlParameter("Titulo", livro.Titulo),
+                new SqlParameter("Autor", livro.Autor),
+                new SqlParameter("Genero", livro.Genero),
+                new SqlParameter("Emprestimo", livro.Emprestimo),
             };
-            return _Repository.ExecuteProcedure("[dbo].[prGET_CUSTUMERS]");
+            _repository.ExecuteNonQueryProcedure("[dbo].[prADD_BOOK]", parameters);
+        }
+
+        public void UpdateBook(Livro livro)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("Id", livro.Id),
+                new SqlParameter("Titulo", livro.Titulo),
+                new SqlParameter("Autor", livro.Autor),
+                new SqlParameter("Genero", livro.Genero),
+                new SqlParameter("Emprestimo", livro.Emprestimo),
+            };
+            _repository.ExecuteNonQueryProcedure("[dbo].[prUPDATE_BOOK]", parameters);
+        }
+
+        public void DeleteBook(int id)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("Id", id),
+            };
+            _repository.ExecuteNonQueryProcedure("[dbo].[prDELETE_BOOK]", parameters);
         }
     }
-
-
 }
