6829815152ace3ed76483c913732fe617b5f74c0
RenanHS2RP <renanoliveiratm@hotmail.com>
2024-06-07 13:45:37 -0300

    feat(UserRepository): Added UserRepository

---
+++ book_manager/book_manager/Repository/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using book_manager.Models;
+
+namespace book_manager.Repository
+{
+    public class UserRepository
+    {
+        public static User Get(string Username, string Password)
+        {
+            var users = new List<User>()
+            {
+                new User
+                {
+                    Id = 1,
+                    Username = "admin",
+                    Password = "admin",
+                    Role = "123",
+                    isAuthorized = true,
+                },
+                new User
+                {
+                    Id = 2,
+                    Username = "Ken",
+                    Password = "123",
+                    Role = "123",
+                    isAuthorized = true,
+                },
+                new User
+                {
+                    Id = 3,
+                    Username = "Ken",
+                    Password = "123",
+                    Role = "123",
+                    isAuthorized = true,
+                }
+            };
+                return users.FirstOrDefault();
+        }
+    }
+}
