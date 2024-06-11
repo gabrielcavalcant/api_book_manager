b715c13c262037e14836eeb1da730e1502d9cb9d
gabrielcavalcant <gabriel_sep@outlook.com>
2024-06-11 13:48:31 -0300

    feat(business): add new procedures and fix existing ones
    - Added new procedures to enhance business logic
    - Fixed issues in existing procedures for improved functionality
    - Ensured better performance and reliability of business operations

--- book_manager/book_manager/Business/Business.cs
+++ book_manager/book_manager/Business/Business.cs
@@ -1,28 +1,65 @@
-﻿using System;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Runtime;
+using book_manager.Models;
 using book_manager.Repository;
 
 namespace book_manager.Business
 {
     public class Business
     {
+        private readonly ImpRepository _repository;
+
         public Business()
         {
-
+            _repository = new ImpRepository();
         }
 
-        public DataTable GetCustumers(int id)
+        public DataTable GetBooks()
         {
-            ImpRepository _Repository = new ImpRepository();
-            SqlParameter[] parameter =
+            return _repository.ExecuteProcedure("[dbo].[prGET_BOOKS]", null);
+        }
+
+        public DataTable AddBook(Livro livro)
+        {
+            SqlParameter[] parameters =
             {
-                new SqlParameter("id", id),
+                new SqlParameter("Titulo", livro.Titulo),
+                new SqlParameter("Autor", livro.Autor),
+                new SqlParameter("Genero", livro.Genero),
+                new SqlParameter("Emprestimo", livro.Emprestimo),
             };
-            return _Repository.ExecuteProcedure("[dbo].[prGET_CUSTUMERS]");
+            return _repository.ExecuteProcedure("[dbo].[prADD_BOOK]", parameters);
+        }
+         public DataTable GetLivrosByGeneroAndTitulo(string genero, string titulo)
+            {
+                SqlParameter[] parameters =
+                {
+                    new SqlParameter("Genero", genero),
+                    new SqlParameter("Titulo", titulo)
+                };
+                return _repository.ExecuteProcedure("[dbo].[prGET_BOOKS_BY_GENERO_AND_TITULO]", parameters);
+            }
+
+        public DataTable UpdateBook(int id, Livro livro)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("Id", livro.Id),
+                new SqlParameter("Titulo", livro.Titulo),
+                new SqlParameter("Autor", livro.Autor),
+                new SqlParameter("Genero", livro.Genero),
+                new SqlParameter("Emprestimo", livro.Emprestimo),
+            };
+            return _repository.ExecuteProcedure("[dbo].[prUPDATE_BOOK]", parameters);
+        }
+
+        public DataTable DeleteBook(int id)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("Id", id),
+            };
+            return _repository.ExecuteProcedure("[dbo].[prDELETE_BOOK]", parameters);
         }
     }
-
-
 }
