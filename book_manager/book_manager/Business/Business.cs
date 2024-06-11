906c0bbc46f0bce9df68a308d51deea9808c1149
RenanHS2RP <renanoliveiratm@hotmail.com>
2024-06-11 17:03:22 -0300

    feat(Business): adicionado todos os metodos de Business para o CRUD

--- book_manager/book_manager/Business/Business.cs
+++ book_manager/book_manager/Business/Business.cs
@@ -1,28 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Runtime;
+using book_manager.Models;
 using book_manager.Repository;
 
 namespace book_manager.Business
 {
     public class Business
     {
+        private readonly ImpRepository _repository;
+
         public Business()
         {
-
+            _repository = new ImpRepository();
         }
 
         public DataTable GetCustumers(int id)
         {
-            ImpRepository _Repository = new ImpRepository();
-            SqlParameter[] parameter =
+            SqlParameter[] parameters =
             {
-                new SqlParameter("id", id),
+                new SqlParameter("@id", id),
             };
-            return _Repository.ExecuteProcedure("[dbo].[prGET_CUSTUMERS]");
+            return _repository.ExecuteProcedure("[dbo].[prGET_CUSTUMERS]", parameters);
+        }
+
+        public DataTable GetLivrosByGeneroAndTitulo(string genero, string titulo)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@Genero", genero),
+                new SqlParameter("@Titulo", titulo),
+            };
+            return _repository.ExecuteProcedure("[dbo].[prGET_BOOKS_BY_GENDER_OR_TITLE]", parameters);
+        }
+
+        public DataTable GetBooks()
+        {
+            return _repository.ExecuteProcedure("[dbo].[prGET_BOOKS]", null);
+        }
+
+        public void AddBook(Livro livro)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@Titulo", livro.Titulo),
+                new SqlParameter("@Autor", livro.Autor),
+                new SqlParameter("@Genero", livro.Genero),
+                new SqlParameter("@Emprestimo", livro.Emprestimo)
+            };
+            _repository.ExecuteProcedure("[dbo].[prADD_BOOK]", parameters);
+        }
+
+        public void UpdateBook(int id, Livro livro)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@Id", id),
+                new SqlParameter("@Titulo", livro.Titulo),
+                new SqlParameter("@Autor", livro.Autor),
+                new SqlParameter("@Genero", livro.Genero),
+                new SqlParameter("@Emprestimo", livro.Emprestimo)
+            };
+            _repository.ExecuteProcedure("[dbo].[prUPDATE_BOOK]", parameters);
+        }
+
+        public void DeleteBook(int id)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@Id", id),
+            };
+            _repository.ExecuteProcedure("[dbo].[prDELETE_BOOK]", parameters);
         }
     }
-
-
 }
