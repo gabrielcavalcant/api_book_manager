59a10aa10493e84363092dc5386cc0f875c0545f
RenanHS2RP <renanoliveiratm@hotmail.com>
2024-06-12 14:51:20 -0300

    refactor(UserRepository): changed admin password

--- book_manager/book_manager/Repository/UserRepository.cs
+++ book_manager/book_manager/Repository/UserRepository.cs
@@ -9,8 +9,8 @@ namespace book_manager.Repository
         // Lista estática simulando um banco de dados
         private static List<User> users = new List<User>
         {
-            new User { Id = 1, Username = "admin", Password = "admin123", Role = "Manager" },
-            new User { Id = 2, Username = "user", Password = "user123", Role = "user" }
+            new User { Id = 1, Username = "admin", Password = "123", Role = "Manager" },
+            new User { Id = 2, Username = "user", Password = "123", Role = "user" }
         };
 
         // Método para obter um usuário pelo nome de usuário e senha
